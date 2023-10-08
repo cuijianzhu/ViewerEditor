1ebedea0ffd140c7a047f476732dfb42f1361c22
gukazma <gukazma@proton.me>
2023-10-08 18:56:24 +0800

    ✨ feat: fileToolbutton

--- Apps/ViewerEditor/Sources/Widgets/FloatTools.cpp
+++ Apps/ViewerEditor/Sources/Widgets/FloatTools.cpp
@@ -1,11 +1,16 @@
 #include "FloatTools.h"
 #include "ui_FloatTools.h"
-
+#include <QMenu>
 FloatTools::FloatTools(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::FloatTools)
 {
     ui->setupUi(this);
+
+    QMenu* menuFile = new QMenu(this);
+    menuFile->addAction(ui->actionImport);
+    menuFile->addAction(ui->actionExport);
+    ui->toolButtonFile->setMenu(menuFile);
 }
 
 FloatTools::~FloatTools()
